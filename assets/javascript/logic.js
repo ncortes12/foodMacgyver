8f81c9309816ae4dbc911e2fd76b43bdc7254fff
ncortes12 <ncortes1415@gmail.com>
2018-06-13 18:24:00 -0700

    added in firebase, wrote skeleton of database functions, linked firebase and js file to html

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,11 +1,42 @@
-var food =  url = "http://api.yummly.com/v1/api/recipes?_app_id=40f36316&_app_key=2ba2f9225692cc4bb5e7d9b14d992e19&q=" + ingredients + "maxTotalTimeInSeconds=" + time +"allowedAllergy[]=" + allergy + "allowedDiet[]=" + diet;
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCywidV9KECB6FaxejYiJIjcPIeEu7I3zs",
+  authDomain: "foodmacgyver-11d8c.firebaseapp.com",
+  databaseURL: "https://foodmacgyver-11d8c.firebaseio.com",
+  projectId: "foodmacgyver-11d8c",
+  storageBucket: "foodmacgyver-11d8c.appspot.com",
+  messagingSenderId: "198176051883"
+};
+firebase.initializeApp(config);
+var food = url = "http://api.yummly.com/v1/api/recipes?_app_id=40f36316&_app_key=2ba2f9225692cc4bb5e7d9b14d992e19&q=" + ingredients + "maxTotalTimeInSeconds=" + time + "allowedAllergy[]=" + allergy + "allowedDiet[]=" + diet;
 
 
 var ingredients;
 var time;
 var allergy;
 var diet;
+var username;
+var database = firebase.database();
 
-var beer = url = "https://api.punkapi.com/v2/beers/?food="+ title +"&per_page=10"
+var beer = url = "https://api.punkapi.com/v2/beers/?food=" + title + "&per_page=10"
 var title;
 
+
+
+database.ref(username).push({
+
+
+
+})
+
+database.ref(username).on("child_added", function (snapshot) {
+  var snap = snapshot.val();
+
+
+})
+
+var replaceSpace = "chicken salad";
+
+var result = replaceSpace.replace(" ", "_");
+
+console.log(result);
