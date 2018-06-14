197b5b6c190f20e15d2be463dade7a6e946263c7
ncortes12 <ncortes1415@gmail.com>
2018-06-13 17:12:51 -0700

    adding in beer api to js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,11 @@
-var url = "http://api.yummly.com/v1/api/recipes?_app_id=40f36316&_app_key=2ba2f9225692cc4bb5e7d9b14d992e19&q=" + ingredients + "maxTotalTimeInSeconds=" + time +"allowedAllergy[]=" + allergy + "allowedDiet[]=" + diet;
+var food =  url = "http://api.yummly.com/v1/api/recipes?_app_id=40f36316&_app_key=2ba2f9225692cc4bb5e7d9b14d992e19&q=" + ingredients + "maxTotalTimeInSeconds=" + time +"allowedAllergy[]=" + allergy + "allowedDiet[]=" + diet;
 
 
 var ingredients;
 var time;
 var allergy;
 var diet;
+
+var beer = url = "https://api.punkapi.com/v2/beers/?food="+ title +"&per_page=10"
+var title;
 
