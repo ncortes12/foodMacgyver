44c4c7eae5ca938543cd70e17bcaa6af0e686c28
Ivan Lewis <ivan.lewis@tonation-nsn.gov>
2018-06-14 21:03:04 -0700

    Updated working AJAX

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,36 +8,63 @@ var config = {
   messagingSenderId: "198176051883"
 };
 firebase.initializeApp(config);
-var food = url = "https://api.edamam.com/search?app_id=bffc1c60&app_key=f34dee8c2c3b557affccc392f878882bq=" + ingredients + "time=1-" + time + "excluded=" + excluded ;
 
+var database = firebase.database();
 
+// Variables for API query
 var ingredients;
 var time;
 var excluded;
 
+// Adding the API into a variable
+var food = "https://api.edamam.com/search?app_id=bffc1c60&app_key=f34dee8c2c3b557affccc392f878882b&q=" + ingredients + "&time=1-" + time + "&excluded=" + excluded;
+
+
+// API GET Request
+$.ajax({
+  url: food,
+  method: "GET"
+})
+
+  // After the data from the AJAX request comes back
+  .then(function (response) {
+
+    // Saving properties into variables
+    var recLabel = response.hits.recipe.label;
+    var recImage = response.hits.recipe.image;
+    var recURL = response.hits.recipe.url;
+    var recIngredients = response.hits.recipe.ingredientsLines;
+    var recTime = response.hits.recipe.totalTimeInSeconds;
+
+    console.log(response);
+
+  });
+
+
+
+// Authentication
 var username;
-var database = firebase.database();
 
 var beer = url = "https://api.punkapi.com/v2/beers/?food=" + title + "&per_page=10"
 var title;
 
 $("#button").on("click", function (event) {
-    event.preventDefault();
-    var ingredientsInputEl = $("#ingredientsInput");
-    var allergyInputEl = $("#allergyInput");
-    var dietInputEl = $("#dietInput");
-    var timeInputEl = $("#timeInput");
-    var ingredients = ingredientsInputEl.val().trim();
-    ingredientsInputEl.val(""); 
-    var allergy = allergyInputEl.val().trim();
-    allergyInputEl.val("");
-    var diet = dietInputEl.val().trim();
-    dietInputEl.val("");
-    var time = timeInputEl.val().trim();
-    timeInputEl.val("");
-    console.log(ingredientsInputEl, allergyInputEl, dietInputEl, timeInputEl);
-  
-    });
+  event.preventDefault();
+  var ingredientsInputEl = $("#ingredientsInput");
+  var allergyInputEl = $("#allergyInput");
+  var dietInputEl = $("#dietInput");
+  var timeInputEl = $("#timeInput");
+  var ingredients = ingredientsInputEl.val().trim();
+  ingredientsInputEl.val("");
+  var allergy = allergyInputEl.val().trim();
+  allergyInputEl.val("");
+  var diet = dietInputEl.val().trim();
+  dietInputEl.val("");
+  var time = timeInputEl.val().trim();
+  timeInputEl.val("");
+  console.log(ingredientsInputEl, allergyInputEl, dietInputEl, timeInputEl);
+
+});
 
 
 database.ref(username).push({
@@ -54,6 +81,6 @@ database.ref(username).on("child_added", function (snapshot) {
 
 var replaceSpace = "chicken salad";
 
-var result = replaceSpace.replace(" ", "_");
+var result = replaceSpace.replace(" ", "-");
 
 console.log(result);
