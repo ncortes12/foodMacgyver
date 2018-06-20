a463e194e6d36a69509c10da364bc371ff95d29f
unknown <thesoupissoup@gmail.com>
2018-06-19 19:04:42 -0700

    buttons style and beer

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -50,12 +50,13 @@ $("#search").on("click", function (event) {
     title = userIngredients.split(" ", 1);
 
     console.log(ingredientsInput, excludedInput, timeInput);
-    
+
   console.log(ingredients, excluded , time );
    
   var food = "https://api.edamam.com/search?app_id=bffc1c60&app_key=f34dee8c2c3b557affccc392f878882b&q=" + ingredients + "&time=1-" + time + "&excluded=" + excluded;
   var beer = "https://api.punkapi.com/v2/beers/?food=" + title + "&per_page=2"
 
+  
   //API GET Request
 $.ajax({
   url: food,
@@ -66,21 +67,32 @@ $.ajax({
     for (var i = 0; i < results.length; i++) {
       // After the data from the AJAX request comes back
 
-
-      // Saving properties into variables
+// Saving properties into variables
+      
       var recLabel = `<div><h1>${results[i].recipe.label}</h1></div>`;
       var recImage = `<img src="${results[i].recipe.image}">`;
-      var recURL = `<div>${results[i].recipe.url}</div>`;
+      var recURL = `<div>
+      <form  action="${results[i].recipe.url}" target="_blank">
+      <input id = "gorecipe" type="submit" value="Go To Recipe"/>
+  </form></div>`;
       var recTime = `<div>${results[i].recipe.totalTimeInSecondsl}</div>`;
 
-      
-      $("#results").append(recLabel, recImage, recURL, recTime);
+      var style = '<div id="reccard" class = "card amber darken-2">'+recLabel+ recImage+recURL+recTime+'</div>';
+      console.log(recImage, recLabel, recTime, recURL);
+      $("#results").append(style);
+      //results[i]
 
+      // $("#gorecipe").on("click", function(){
+      //   window.open(results[i].recipe.url)
+      // });
+
+      // reccard,recLabel, recImage, recURL, recTime
       // console.log(recLabel, recImage, recURL, recTime);
-
+      console.log(food);
       // console.log(response);
 
     }
+
     $.ajax({
       url: beer,
       method: "GET"
@@ -89,14 +101,19 @@ $.ajax({
         var beerResult = beerResponse
       for (var j=0; j < beerResult.length; j++){
         
-        var beerName = beerResult[j].name;
-        var tagline = beerResult[j].tagline;
-        var description = beerResult[j].description;
-        var beerImg = beerResult[j].image_url;
+        var beerName = `<div><h1>${beerResult[j].name}</h1></div>`;
+        var tagline = `<div><h2>${beerResult[j].tagline}</h2></div>`;
+        var description = `<div>${beerResult[j].description}</div>`;
+        var beerImg = `<img src="${beerResult[j].image_url}" width="100px" height"200px">`;
         
         console.log(beerName, tagline, description, beerImg );
        
-  
+
+        var alcohol='<div class = "card amber darken-2">'+beerName+tagline+description+description+beerImg+'</div>'
+       
+        
+        $("#beer").append(alcohol);
+        // beerName,tagline,description,beerImg
       }})
   })
 
